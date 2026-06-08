f29532c4cf2b82b3eef3692249f49a43b4b01c89
Danila <Danila@DESKTOP-44LP0PV>
2026-06-08 21:17:40 +0300

    Mad client heal

--- Client/main.cpp
+++ Client/main.cpp
@@ -20,6 +20,7 @@ using namespace std;
 #define PORT "27015"
 #define BUFFER_LENGTH 1500
 
+CHAR recvbuffer[BUFFER_LENGTH] = {};
 
 VOID Receive(SOCKET connect_socket);
 
@@ -112,12 +113,12 @@ void main()
 		}
 		cout << "Bytes sent: " << iResult << endl;
 
-	
+	  
 	  ZeroMemory(sendbuffer, BUFFER_LENGTH);
 	  SetConsoleCP(1251);
 	  cin.getline(sendbuffer, BUFFER_LENGTH);
 	  SetConsoleCP(866);
-	} while (strcmp(sendbuffer, "exit") != 0);
+	} while (strcmp(sendbuffer, "exit") != 0 && strcmp(recvbuffer, DECLINE_MESSAGE) != 0);
 
 	iResult = shutdown(connect_socket, SD_SEND);
 	if (iResult == SOCKET_ERROR)
@@ -137,7 +138,7 @@ VOID Receive(SOCKET connect_socket)
 	INT iResult = 0;
 	DWORD dwError = 0;
 	CHAR szError[256] = {};
-	CHAR recvbuffer[BUFFER_LENGTH] = {};
+	//CHAR recvbuffer[BUFFER_LENGTH] = {};
 	do
 	{
 		ZeroMemory(recvbuffer, sizeof(recvbuffer));
@@ -148,10 +149,11 @@ VOID Receive(SOCKET connect_socket)
 		if (iResult > 0) cout << recvbuffer << "(" << iResult << " Bytes)" << endl;
 		//else if (result == 0) cout << "Connection closed" << endl;
 		else cout << FormatLastError(WSAGetLastError(), szError) << endl; //cout << "Receive failed:\t" << WSAGetLastError() << endl;
-	} while (iResult > 0);
+	} while (strcmp(recvbuffer, DECLINE_MESSAGE) != 0);
 	if (strcmp(recvbuffer, DECLINE_MESSAGE) == 0)
 	{
-		system("PAUSE");
+		cout << "для выхода нажмите Enter " << endl;
+		//system("PAUSE");
 		//break;
 	}
 }
